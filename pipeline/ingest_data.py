d1457a87a357f9733120fc322100cf2d627b219f
daniel-lee-wilkinson <130903659+daniel-lee-wilkinson@users.noreply.github.com>
2026-03-22 16:16:19 +0000

    docker image postgresql in progres

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 dtype = {
     "VendorID": "Int64",
@@ -27,25 +28,25 @@ parse_dates = [
 ]
 
 
-def run():
-    pg_user = "root"
-    pg_pass = "root"
-    pg_host = "localhost"
-    pg_db = "ny_taxi"
-    pg_port = 5432
-    chunksize = 100000
-
-    year = 2021
-    month = 1
-
-    target_table = "yellow_taxi_data"
-
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
     base_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
     url = f"{base_url}/yellow_tripdata_{year}-{month:02d}.csv.gz"
 
     engine = create_engine(
         f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
     )
+
+    print(f"pg_user = {pg_user}, pg_host = {pg_host}, pg_port = {pg_port}, pg_db = {pg_db}, year = {year}, month = {month}, chunksize = {chunksize}, target_table = {target_table}")
 
     df_iter = pd.read_csv(
         url,
@@ -66,7 +67,7 @@ def run():
                 index=False
             )
             first = False
-            print("Table created")
+            print(f"Table created: {target_table}")
 
         df_chunk.to_sql(
             name=target_table,
@@ -80,3 +81,47 @@ def run():
 
 if __name__ == "__main__":
     run()
+
+
+""" EXAMPLE RUN COMMAND:
+uv run python ingest_data.py \
+  --pg-user=root \
+  --pg-pass=root \
+  --pg-host=localhost \
+  --pg-port=5432 \
+  --pg-db=ny_taxi \
+  --target-table=yellow_taxi_trips_2021_1 \
+  --year=2021 \
+  --month=1 \
+  --chunksize=100000
+"""
+
+# Running it inside docker:
+
+""" 
+# taxi_ingest:v001 is the container with all dependencies for ingest_data.py, built from Dockerfile in this repo. Pushes data to the postgres.
+
+# Run PostgreSQL on the network
+docker run -it \
+  -e POSTGRES_USER="root" \
+  -e POSTGRES_PASSWORD="root" \
+  -e POSTGRES_DB="ny_taxi" \
+  -v ny_taxi_postgres_data:/var/lib/postgresql \
+  -p 5432:5432 \
+  --network=pg-network \
+  --name pgdatabase \
+  --network=pg-network \
+  postgres:18
+
+# In another terminal, run pgAdmin on the same network
+docker run -it \
+  -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+  -e PGADMIN_DEFAULT_PASSWORD="root" \
+  -v pgadmin_data:/var/lib/pgadmin \
+  -p 8085:80 \
+  --network=pg-network \
+  --name pgadmin \
+  --network=pg-network \
+  dpage/pgadmin4
+
+    """
