f368900a5ba7ab4dd7d05a1aecedc2209cb957ca
daniel-lee-wilkinson <130903659+daniel-lee-wilkinson@users.noreply.github.com>
2026-03-23 15:43:49 +0000

    added docker-compose.yaml file

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -123,3 +123,20 @@ docker run -it \
   dpage/pgadmin4
 
     """
+
+"""
+# it's pipeline_default
+# now run the script:
+docker run -it \
+  --network=pipeline_default \
+  taxi_ingest:v001 \
+    --pg-user=root \
+    --pg-pass=root \
+    --pg-host=pgdatabase \
+    --pg-port=5432 \
+    --pg-db=ny_taxi \
+    --target-table=yellow_taxi_trips_2021_1 \
+    --year=2021 \
+    --month=1 \
+    --chunksize=100000
+"""
